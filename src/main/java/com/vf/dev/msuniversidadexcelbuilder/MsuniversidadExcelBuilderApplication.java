0f7ecd0d1c3caeb0a76dee60d62a6244d6476cb8
VFALAN <alanvillafan95@gmail.com>
2023-08-21 15:05:07 -0600

    comit initial

---
+++ src/main/java/com/vf/dev/msuniversidadexcelbuilder/MsuniversidadExcelBuilderApplication.java
@@ -0,0 +1,13 @@
+package com.vf.dev.msuniversidadexcelbuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsuniversidadExcelBuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsuniversidadExcelBuilderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vf/dev/msuniversidadexcelbuilder/MsuniversidadExcelBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vf.dev.msuniversidadexcelbuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsuniversidadExcelBuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
